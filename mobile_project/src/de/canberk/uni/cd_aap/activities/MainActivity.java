70a3a6ecd9e24bfe6238fe50c9dc434940f9199a
canberk.demirkan <canberk.demirkan@gmail.com>
2013-12-28 19:09:27 +0100

    test commit

--- mobile_project/src/de/canberk/uni/cd_aap/activities/MainActivity.java
+++ mobile_project/src/de/canberk/uni/cd_aap/activities/MainActivity.java
@@ -25,7 +25,7 @@ import android.widget.TextView;
 import android.widget.Toast;
 import de.canberk.uni.cd_aap.R;
 
-@SuppressLint("NewApi")
+@SuppressLint("NewApi") // Test
 public class MainActivity extends Activity {
 
 	private StrictMode.ThreadPolicy policy = new StrictMode.ThreadPolicy.Builder()
